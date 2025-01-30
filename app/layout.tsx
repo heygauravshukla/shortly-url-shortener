98d08d53b785b35b15413508eddf0641be254c27
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-01-30 20:49:40 +0530

    Add Poppins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Shortly - URL Shortener",
@@ -13,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.variable}>
       <body className="antialiased">{children}</body>
     </html>
   );

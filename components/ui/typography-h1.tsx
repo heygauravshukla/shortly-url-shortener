574cf8513edd853d4bdb71396bdbdca460ee6522
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-01-31 14:31:38 +0530

    Add TypographyH1 component

---
+++ components/ui/typography-h1.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+interface TypographyH1Props extends React.HTMLAttributes<HTMLHeadingElement> {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default function TypographyH1({
+  className,
+  children,
+  ...props
+}: TypographyH1Props) {
+  return (
+    <h1
+      className={cn(
+        "text-5xl/snug font-bold -tracking-1 lg:text-6xl/tight",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </h1>
+  );
+}

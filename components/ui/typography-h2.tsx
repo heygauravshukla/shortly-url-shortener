e78980ec26b6f2bdcd5dce2f63e6b90943d28e8a
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-01-31 15:04:40 +0530

    Add TypographyH2 component

---
+++ components/ui/typography-h2.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+interface TypographyH2Props extends React.HTMLAttributes<HTMLHeadingElement> {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default function TypographyH2({
+  className,
+  children,
+  ...props
+}: TypographyH2Props) {
+  return (
+    <h2
+      className={cn("text-4xl font-bold -tracking-2 lg:text-5xl", className)}
+      {...props}
+    >
+      {children}
+    </h2>
+  );
+}

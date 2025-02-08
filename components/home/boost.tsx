f5c822985199a17f067958cb71d5d3d5dd8adb9e
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-02-08 14:39:56 +0530

    Add background images to Boost component

--- components/home/boost.tsx
+++ components/home/boost.tsx
@@ -4,7 +4,10 @@ import Wrapper from "../ui/wrapper";
 
 export default function Boost() {
   return (
-    <section aria-labelledby="boost-heading" className="bg-dark-violet">
+    <section
+      aria-labelledby="boost-heading"
+      className="bg-dark-violet bg-[url('/backgrounds/bg-boost-mobile.svg')] bg-cover bg-center bg-no-repeat lg:bg-[url('/backgrounds/bg-boost-desktop.svg')]"
+    >
       <Wrapper className="grid place-items-center gap-4 pb-[5.5rem] pt-24 lg:gap-5 lg:py-14">
         <TypographyH2 id="boost-heading" className="text-center text-white">
           Boost your links today
